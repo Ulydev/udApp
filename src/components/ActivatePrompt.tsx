7215d63be020eb7564738caa3843f5acb3647e6e
ulydev <ulydev@live.fr>
2020-09-21 20:06:20 +0200

    set img alt logo

--- src/components/ActivatePrompt.tsx
+++ src/components/ActivatePrompt.tsx
@@ -8,7 +8,7 @@ const ActivatePrompt = () => {
     return (
         <Container>
             <div className="flex flex-col space-y-4 items-center">
-                <img src="/logo192.png" className="w-24 h-24 rounded-full" />
+                <img src="/logo192.png" alt="logo" className="w-24 h-24 rounded-full" />
                 <span className="text-black-700 font-bold text-lg text-center">Please connect your wallet before proceeding.</span>
                 <button
                     className="flex-1 p-2 px-4 border hover:text-black hover:bg-white bg-black text-white font-bold rounded-full w-full"
